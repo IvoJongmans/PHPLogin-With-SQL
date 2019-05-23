6f881af6601c372b3ccdb3a89b43e21ab1b7caa8
Ivo Jongmans <ivojongmans@gmail.com>
2019-05-23 12:10:10 +0200

    create new user is working

--- createuser.php
+++ createuser.php
@@ -3,11 +3,11 @@ require('dbconnect.php');
 if (isset($_POST['new_user_id']) and isset($_POST['new_user_pass'])){	
 $newusername = $_POST['new_user_id'];
 $newpassword = $_POST['new_user_pass'];
-$sql = "INSERT INTO userdata (username, password) VALUES ($newusername, $newpassword)";
+$sql = "INSERT INTO userdata (username, password) VALUES ('$newusername', '$newpassword')";
 $addnewuser = $conn->prepare($sql);
 //Ik moet de parameters nog binden. Excuus.
 $addnewuser->execute();
-$count = $adduser->rowCount();
+$count = $addnewuser->rowCount();
 if ($count == 1){
 echo "<script type='text/javascript'>alert('User added!')</script>";
 }
