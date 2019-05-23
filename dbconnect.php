795785f082907ff53e57b78280220a70a5853f72
Ivo Jongmans <ivojongmans@gmail.com>
2019-05-23 11:10:47 +0200

    established connection to the DB

---
+++ dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+require_once "login.php";
+try {
+    $conn = new PDO("mysql:host=$hn;dbname=$db", $un, $pw);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully <br/>"; 
+    }
+catch(PDOException $e)
+    {
+      echo "Connection failed: " . $e->getMessage();
+    }
+?>
