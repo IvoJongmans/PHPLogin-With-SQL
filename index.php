597083c0318ece6dfcc1c761333cc194a2188cee
Ivo Jongmans <ivojongmans@gmail.com>
2019-05-23 19:28:55 +0200

    started styling create user page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html >
 <html>
 <head>
-<title>Simple Login</title>
+<title>Login</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai&display=swap" rel="stylesheet">
@@ -16,7 +16,6 @@
         <i class="fab fa-joomla fa-3x" style="padding-top:25px;padding-bottom:25px;"></i>
     </div>
 </div>
-
 <div class="container">
     <div class="col-sm-offset-3 col-sm-9">
   <h2>Login</h2>
@@ -25,13 +24,13 @@
     <div class="form-group">
       <label class="control-label col-sm-3" for="user_id">Username:</label>
       <div class="col-sm-6 col-sm-offset-right-3">
-        <input type="text" class="form-control" id="User_id" placeholder="Enter username" name="user_id" required>
+        <input type="text" class="form-control" id="User_id" name="user_id" required>
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-3" for="user_pass">Password:</label>
       <div class="col-sm-6 col-sm-offset-right-3">          
-        <input type="password" class="form-control" id="user_pass" placeholder="Enter password" name="user_pass" required>
+        <input type="password" class="form-control" id="user_pass" password" name="user_pass" required>
       </div>
     </div>
     <div class="form-group">        
@@ -43,25 +42,17 @@
     </div>
     <div class="form-group">        
       <div class="col-sm-offset-3 col-sm-9">
-        <button type="submit" class="btn btn-default">Submit</button>
+        <button type="submit" class="btn btn-default">Login</button>
         <!-- hier moet nog een reset button bij -->
       </div>
     </div>
   </form>
 </div>
-
-<!-- <div>
-<h3>Simple login</h3>
-    <form id="login-form" method="post" action="controle.php" >            
-                <label for="user_id">Username:</label>
-                <input type="text" name="user_id" id="user_id">           
-                <label for="user_pass">Password:</label>
-                <input type="password" name="user_pass" id="user_pass">			
-                <input type="submit" value="Submit">
-                <input type="reset" value="Reset">        
-    </form>
-</div> -->
-
+<div class="container">
+    <div class="col-sm-offset-3 col-sm-6">
+        <p>New to our page? Click <a href=createuser.html>here</a> to create an account</p>
+    </div>
+</div>
 </body>
 </html>
 
