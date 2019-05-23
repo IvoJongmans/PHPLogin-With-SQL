117522cdb6918d870e6e09b0847f3beda6c83765
Ivo Jongmans <ivojongmans@gmail.com>
2019-05-23 16:28:36 +0200

    added a bootstrap navbar

--- succes.php
+++ succes.php
@@ -6,12 +6,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
     <title>Document</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
     <script src="getuserdata.js" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
 </head>
 <body>
+<nav class="navbar navbar-expand-sm bg-light">
+
+<!-- Links -->
+<ul class="navbar-nav">
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link 1</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link 2</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link 3</a>
+  </li>
+</ul>
+
+</nav>
+    
     <div id="userres">
 
     </div>
